78c9364a8a4a61905da7907cfe427c1c619bf23c
Shivesh Mohamed <shiveshmohamed1234@gmail.com>
2023-04-04 19:44:20 -0400

    checkouts like nearly done, ig. Needs the 'progress bar' and validation

---
+++ javascript/cart.js
@@ -0,0 +1,10 @@
+// transitions between the cart and checkout content
+function cartCheckoutTransition(n) {
+
+    if(n == 0) {
+      window.location.href = "../html/checkout.html"
+    } else {
+      window.location.href = "../html/cart.html"
+    }
+  
+ }
