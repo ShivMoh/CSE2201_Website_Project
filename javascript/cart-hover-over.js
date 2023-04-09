8d5f68e98399cacc4a5c8018750c79793165d6c8
Shivesh Mohamed <shiveshmohamed1234@gmail.com>
2023-04-09 17:48:28 -0400

    cleaning

--- javascript/cart-hover-over.js
+++ javascript/cart-hover-over.js
@@ -13,19 +13,6 @@ deliveryDropdown.forEach(element => {
     });
 });
 
-// cartLink.mouseenter = () => {
-//   console.log("Hello?")
-// }
-// deliveryDropdown.addEventListener('mouseenter', () => {
-// cartLink.dataset.hovering = 'true';
-// });
-
-// deliveryDropdown.addEventListener('mouseleave', () => {
-// cartLink.dataset.hovering = 'false';
-// });
-
-
-
 cartLink.addEventListener('click', (event) => {
 if (cartLink.dataset.hovering === 'true') {
   event.preventDefault();
